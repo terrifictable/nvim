c029a4f7bb5a22c767873d668bb3b18667ac9b38
TerrificTable <omegagaming189@gmail.com>
2022-12-25 23:37:22 +0100

    coc configs

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["coc.nvim"] = {
+    loaded = true,
+    path = "/home/terrific/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    url = "https://github.com/neoclide/coc.nvim"
+  },
   harpoon = {
     loaded = true,
     path = "/home/terrific/.local/share/nvim/site/pack/packer/start/harpoon",
