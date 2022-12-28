c0575148fbe8cef2b4dac7aea29f94b533ad981b
TerrificTable <omegagaming189@gmail.com>
2022-12-28 23:44:56 +0100

    added configurations for bufferline.nvi

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,5 +1,41 @@
 
-require("bufferline").setup{}
+require('bufferline').setup {
+    options = {
+        mode = "buffers", -- set to "tabs" to only show tabpages instead
+        buffer_close_icon = '',
+        modified_icon = '●',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+        max_name_length = 20,
+        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        truncate_names = true, -- whether or not tab names should be truncated
+        diagnostics = "nvim_lsp",
+        diagnostics_update_in_insert = false,
+        -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+                local sym = e == "error" and " "
+                or (e == "warning" and " " or " " )
+                s = s .. sym .. n
+            end
+            return s
+        end,
+        offsets = {
+            {
+                filetype = "neo-tree",
+                text = "File Explorer",
+                text_align = "center",
+                separator = true
+            }
+        },
+        color_icons = true, -- whether or not to add the filetype icon highlights
+        show_buffer_icons = true, -- disable filetype icons for buffers
+        show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
+        show_duplicate_prefix = false, -- whether to show duplicate buffer prefix
+        sort_by = 'insert_after_current'
+    }
+}
 
 vim.keymap.set("n", "bp", vim.cmd.BufferLinePick)
 vim.keymap.set("n", "br", vim.cmd.BufferLineCloseRight)
