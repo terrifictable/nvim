40b853b9252276209bbc64c165638700a7279fa4
TerrificTable <omegagaming189@gmail.com>
2023-01-07 17:27:51 +0100

    did some rather ueless changes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,12 @@
 
+
 function DoColors(color)
 	color = color or vim.colorscheme
-    if (color == "carbonfox") then -- there is probably a better way to do this, but i dont care
-        color = nil
-    end
+
     vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "#0e0e0e" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#0e0e0e" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#0e0e0e" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#0e0e0e" })
 end
 
 
